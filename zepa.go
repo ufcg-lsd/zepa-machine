ab6076cc7accd753b8aaa26b36e9e80fddbb9b49
Filipe José <filipe1417@gmail.com>
2024-09-05 18:24:46 -0300

    initial structs the machine

---
+++ zepa.go
@@ -0,0 +1,28 @@
+package main
+
+type GeneralRegisters struct {
+	w0, w1, w2, w3, w4, w5 byte
+}
+
+type SpecialRegisters struct {
+	pc  byte
+	sp  byte
+	ir  byte
+	sr  byte
+	mdr byte
+	mar byte
+}
+
+type Memory struct {
+	data [1024]byte
+}
+
+type Machine struct {
+	genRegs  GeneralRegisters
+	specRegs SpecialRegisters
+	memory   Memory
+}
+
+func main() {
+
+}
