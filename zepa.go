07834934ff54c54d6b39ef9db5f71403d069d8fa
Filipe José <filipe1417@gmail.com>
2024-09-11 17:23:37 -0300

    Revert "Revert "basic structure for instruction cycle""

--- zepa.go
+++ zepa.go
@@ -22,8 +22,24 @@ type Machine struct {
 	registers map[Register]byte
 }
 
-func NewMachine(memoryBytes int) *Machine {
+func (m *Machine) fetch() {
+	m.registers[ir] = m.memory[m.registers[pc]]
+	m.registers[pc] += 1
+}
 
+func (m *Machine) decode() {}
+
+func (m *Machine) execute() {}
+
+func (m *Machine) boot() {
+	for {
+		m.fetch()
+		m.decode()
+		m.execute()
+	}
+}
+
+func NewMachine(memoryBytes int) *Machine {
 	machine := &Machine{
 		memory:    make([]byte, memoryBytes),
 		registers: make(map[Register]byte),
