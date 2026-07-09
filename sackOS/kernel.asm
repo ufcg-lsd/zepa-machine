a22adef5ca23ab431d82f7779cb5b2af6decf210
lucasaraujomedeiros <lucasmedeirosjpa@gmail.com>
2026-07-08 23:15:05 -0300

    feat: add kernel setup function

--- sackOS/kernel.asm
+++ sackOS/kernel.asm
@@ -9,3 +9,50 @@ _getPID:
     STORE W0 W1 ; stores RUNNING_PID on pcb[RUNNING_PID].w9
 
     JUMP _schedule
+
+_setup:
+    LOAD W1 #KERNEL_MAX_MEMORY_CNST
+    LOAD W2 #BUFFER_CNST
+    ADD W3 W1 W2 ; W3 -> KERNEL_MAX_MEMORY + BUFFER
+
+    SUB W0 LIMIT W3 ; W0 -> USER_MEMORY = limit - (KERNEL_MAX_MEMORY + BUFFER)
+    LOAD W2 #PARTITION_SIZE_CNST
+
+    DIV W3 W0 W2 ; W3 -> NUM_PARTITIONS = user_memory / PARTITION_SIZE
+
+    LOAD W4 #PCBADRESS
+    ADD W4 W4 #72 ; gets W4 to pcb_v[0].BASE address
+
+   ; W1 will acumulate PARTITION_SIZE * i through the loop
+
+    MV W6 #0 ; W6 -> pid = 0
+    ; for (int pid = 0, pid < NUM_PARTITIONS; pid++)
+
+    ADD W6 W6 1
+    CMP W6 W3 
+    BEQ _setup_registers ; loop conditions
+
+    STORE W1 W4 ; pcb_v[pid].BASE = KERNEL_MAX_MEMORY + pid * PARTITION_SIZE
+    
+    ADD W4 W4 #4 ; W4 = pcb_v[pid].LIMIT address
+
+    ADD W7 W1 W2 ; W7 = BASE + PARTITION_SIZE
+
+    STORE W7 W4 ; pcb_v[pid].LIMIT = BASE + PARTITION_SIZE
+
+    ; setup variables to next Iteration
+
+    ADD W4 W4 #80 ; W4 = pcb_v[pid+1].BASE address
+    ADD W1 W1 W2 ; W1 += PARTITION_SIZE
+
+
+_setup_registers:
+    MV SP #SP_ADDRESS_CNST
+    MV ESA #EXCEPTION_SUPERVISOR_ADDRESS_CNST
+    MV EPC #LOOP_ADDRESS_CNST
+    MV ESR #16 ; enable interruptions
+    
+    MRET ; go to infinite loop, waiting for program inputs
+
+
+
