8dd3cc5cba58f2c4c2f68662acc5e32a12122339
lucasaraujomedeiros <lucasmedeirosjpa@gmail.com>
2026-07-12 00:23:11 -0300

    feat: finish wait function

--- sackOS/kernel.asm
+++ sackOS/kernel.asm
@@ -178,6 +178,7 @@ _wait:
     
     LOAD W3 W0 ; w3 = pcb_v[RUNNING_PID].LIMIT
 
+    ADD W7 W2 W9 ; w7 = pcb_v[running_pid].BASE + status_addr
     ADD W2 W2 W9 ; w2 = pcb_v[running_pid].BASE + status_addr
     
     MV W8 #4
@@ -188,7 +189,7 @@ _wait:
     ; fault_int()
 
 
-    CMP W2 W2 
+    CMP W2 W3
     BGT _falt_int 
 
 
@@ -209,8 +210,10 @@ _wait:
     LOAD W3 W2 ; w3 = curr_child = pcb_v[running_pid].childPID
     LDD W0 #PCBADRESS_CNST ;  w0 points to pcb_v[0] first byte
     ; 
-    ; do
+    ; starts loop
+    JUMP _waitLoop
 
+_waitLoop:
     ;calculate pcb_v[curr_child]
 
     MV W8 #84
@@ -224,7 +227,122 @@ _wait:
     MV W8 #0b01000000000000000000000000000000
     AND W8 W8 W1 
     CMP W1 W8
+    BEQ #4 ;
+
+    ;if !pcb_v[curr_child].is_zombie:
+    ; curr_child = pcb_v[curr_child].next_sibling
+
+    MV W8 #68
+    SUB W3 W1 W8 ; w3 points to pcb_v[curr_child].next_siblingPID
+    JUMP _waitLoop ; goes to next Iteration
+
+    ; else (pcb_v[curr_child].is_zombie)
+
+    MV W8 #0b01111111111111111111111111111111
+    AND W8 W8 W1 
+    STORE W8 W1 ; sets pcb_v[curr_child].is_mapped = 0
+
+
     
+    ; w0 points to pcb_v[0] first byte
+    ; w1 points to pcb_v[curr_child].flags
+    ; w2 points to pcb_v[running_pid].child
+    ; w3 = curr_child
+    ; w7 = pcb_v[running_pid].BASE + status_addr
+
+    MV W8 #56
+    ADD W4 W2 W8 ; w4 points to pcb_v[running_pid].w9
+    STORE W3 W4 ; pcb_v[running_pid].w9 = curr_child
+
+    MV W8 #26 
+    SUB W1 W1 W8 ; w1 points to pcb_v[curr_child].w9
+
+    STORE W1 W7
+
+    MV W8 #44
+    SUB W1 W1 W8 ; w1 points to pcb_v[curr_child].next_sibling
+
+    CMP W2 W3 ; if curr_child == pcb_v[running_pid].child
+    BEQ #2
+
+    JUMP #2
+
+    STORE W1 W2 ; pcb_v[running_pid].child = pcb_v[curr_child].next_sibling
+
+    ; else 
+
+    MV W8 #4
+    SUB W4 W1 W8 ; w4 points to pcb_v[running_pid].prev_sibling
+
+    MV W6 #84 ; 84 bytes each pcb
+    MUL W5 W4 W6 ; 
+    ADD W5 W0 W5 ; w5 points to pcb_v[pcb_v[running_pid].prev_sibling] first byte
+
+    MV W8 #12
+    ADD W5 W5 W8 ; w5 points to pcb_v[pcb_v[running_pid].prev_sibling].next_sibling
+
+    STORE W1 W5 ; pcb_v[pcb_v[running_pid].prev_sibling].next_sibling = pcb_v[curr_child].next_sibling
+
+    MV W8 #0b01111111111111111111111111111111
+    CMP W1 W8 ; if pcb_v[curr_child].next_sibling == -1 
+    BEQ _schedule
+
+    ; else
+
+    MUL W5 W1 W6 
+    ADD W5 W0 W5 ; w5 points to pcb_v[pcb_v[running_pid].next_sibling] first byte
+
+    MV W8 #8
+    ADD W5 W5 W8 ; w5 points to pcb_v[pcb_v[running_pid].next_sibling].prev_sibling
+
+    MV W8 #4
+    SUB W1 W1 W8 ; w1 = pcb_v[curr_child].prev_sibling
+
+    STORE W1 W5 ; pcb_v[pcb_v[running_pid].next_sibling].prev_sibling = pcb_v[curr_child].prev_sibling
+
+    JUMP _schedule
+
+    ; w2 = pcb_v[running_pid].child
+
+    MV W8 #12
+    ADD W2 W2 W8 ; w2 = pcb_v[running_pid].status_add
+    STORE W9 W2 ; pcb_v[running_pid].status_addr = status_addr
+
+    MV W8 #64
+    ADD W2 W2 W8 ; w2 points to pcb_v[running_pid].flags
+
+    LOAD W4 W2 ; w4 = pcb_v[running_pid].flags
+
+    ; pcb_v[running_pid].scheduler_state = blocked
+    ; pcb_v[running_pid].is_waiting = 1
+    ; same to set flags byte to  0bXX110XXX
+
+    
+    ; set 1's 
+    MV W8 #0b00110000
+    OR W4 W4 W8
+
+    MV W8 #0b11110111
+    AND W4 W4 W8
+
+    STORE W4 W2 ;
+
+    JUMP _schedule
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 
 
 
