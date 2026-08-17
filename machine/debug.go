dba7e4fb4826a14b9989e7968013d9ddd2f65820
Nicholas Cartaxo <nicholas.cartaxo.de.souza.rangel.alves@ccc.ufcg.edu.br>
2026-08-17 17:42:57 -0300

    fix: memory decompiling

--- machine/debug.go
+++ machine/debug.go
@@ -914,18 +914,25 @@ func (m *Machine) debugProcessTableTo(out io.Writer) {
 
 var opNames = map[byte]string{
 	0: "MV", 1: "AND", 2: "OR", 3: "XOR",
-	4: "ADD", 5: "SUB", 6: "MUL", 7: "UDIV",
-	8: "SDIV", 9: "CMP", 10: "JUMP", 11: "JMPR",
-	12: "BEQ", 13: "BLT", 14: "BGT",
-	15: "LOAD", 16: "STORE", 17: "LDD", 18: "STRD",
-	19: "LDB", 20: "LDSB", 21: "STRB", 22: "MRET",
-	23: "SYSCALL",
+	4: "SHL", 5: "SHA", 6: "ADD", 7: "SUB",
+	8: "MUL", 9: "UDIV", 10: "SDIV", 11: "CMP",
+	12: "JUMP", 13: "JMPR", 14: "BEQ", 15: "BLT",
+	16: "BGT", 17: "LOAD", 18: "STORE", 19: "LDD",
+	20: "STRD", 21: "LDB", 22: "LDSB", 23: "STRB",
+	24: "MRET", 25: "SYSCALL",
 }
 
 var regNames = [22]string{
 	"w0", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9",
 	"pc", "sp", "ir", "sr", "mdr", "mar",
 	"ecr", "esa", "esr", "epc", "base", "limit",
+}
+
+func regName(index byte) string {
+	if int(index) >= len(regNames) {
+		return fmt.Sprintf("r%d", index)
+	}
+	return regNames[index]
 }
 
 func decodeInstruction(inst uint32) string {
@@ -935,40 +942,33 @@ func decodeInstruction(inst uint32) string {
 		return ".word 0x" + fmt.Sprintf("%08X", inst)
 	}
 
-	rType := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16, 19, 20, 21}
-	isRType := false
-	for _, op := range rType {
-		if opcode == op {
-			isRType = true
-			break
-		}
-	}
+	rd := byte((inst >> 21) & 0x1F)
+	rs1 := byte((inst >> 16) & 0x1F)
+	rs2 := byte((inst >> 11) & 0x1F)
+	imm := (inst >> 5) & 0xFFFF
 
-	if isRType {
-		rd := byte((inst >> 21) & 0x1F)
-		rs1 := byte((inst >> 16) & 0x1F)
-		rs2 := byte((inst >> 11) & 0x1F)
-		if opcode == 9 {
-			return fmt.Sprintf("%s %s, %s", name, regNames[rs1], regNames[rs2])
-		}
-		if opcode == 11 || opcode == 15 || opcode == 16 || opcode == 19 || opcode == 20 || opcode == 21 {
-			return fmt.Sprintf("%s %s, [%s]", name, regNames[rd], regNames[rs1])
-		}
-		return fmt.Sprintf("%s %s, %s, %s", name, regNames[rd], regNames[rs1], regNames[rs2])
-	}
-
-	rs1rd := byte((inst >> 21) & 0x1F)
-	imm := int16((inst >> 5) & 0xFFFF)
-	if opcode == 10 || opcode == 12 || opcode == 13 || opcode == 14 {
-		return fmt.Sprintf("%s %+d", name, imm)
-	}
-	if opcode == 22 {
+	switch opcode {
+	case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10: // AND, OR, XOR, SHL, SHA, ADD, SUB, MUL, UDIV, SDIV
+		return fmt.Sprintf("%s %s, %s, %s", name, regName(rd), regName(rs1), regName(rs2))
+	case 11: // CMP
+		return fmt.Sprintf("%s %s, %s", name, regName(rs1), regName(rs2))
+	case 13: // JMPR
+		return fmt.Sprintf("%s %s", name, regName(rs1))
+	case 17, 18, 21, 22, 23: // LOAD, STORE, LDB, LDSB, STRB
+		return fmt.Sprintf("%s %s, %s", name, regName(rs1), regName(rs2))
+	case 12, 14, 15, 16: // JUMP, BEQ, BLT, BGT
+		return fmt.Sprintf("%s %+d", name, int16(imm))
+	case 0: // MV
+		return fmt.Sprintf("%s %s, #%d", name, regName(rd), int16(imm))
+	case 19, 20: // LDD, STRD
+		return fmt.Sprintf("%s %s, #%d", name, regName(rd), imm)
+	case 24: // MRET
 		return "MRET"
-	}
-	if opcode == 23 {
+	case 25: // SYSCALL
 		return fmt.Sprintf("SYSCALL #%d", imm)
+	default:
+		return ".word 0x" + fmt.Sprintf("%08X", inst)
 	}
-	return fmt.Sprintf("%s %s, #%d", name, regNames[rs1rd], imm)
 }
 
 func (m *Machine) GetMemoryViewString() string {
